e7b1af17ee5bd5e05a7a559a4c96ad01119c1899
Jamie Yoon <rkawk3316@gmail.com>
2019-03-11 15:40:12 +0200

    Added share ride

---
+++ src/interface/ride.ts
@@ -0,0 +1,7 @@
+export interface RideDetails {
+  start?: string;
+  destination?: string;
+  timeAdded?: string;
+  timeReached?: string;
+  rideDescription?: string;
+}
--- src/pages/shareride/shareride.ts
+++ src/pages/shareride/shareride.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController, NavController, NavParams } from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 import { Pic, SearchParam } from '../../interface/media';
 import { Observable } from 'rxjs';
 import { selectValueAccessor } from '@angular/forms/src/directives/shared';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { RideDetails } from '../../interface/ride';
 
 /**
  * Generated class for the ShareridePage page.
@@ -18,43 +20,112 @@ import { selectValueAccessor } from '@angular/forms/src/directives/shared';
 })
 export class ShareridePage {
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private dataProvider: DataProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private dataProvider: DataProvider,
+    private camera: Camera,
+    private  actionSheetCtrl: ActionSheetController) {
   }
 
-  ridesArray: Observable<Pic[]>;
-  ride = {
-    start: '',
-    destination: ''
+  filedata = '';
+  rideDetail: RideDetails = {
+    start: null,
+    destination: null,
+    timeAdded: null,
+    timeReached: null,
+    rideDescription: null
   };
+  rideBlob: Blob;
 
-  ionViewDidEnter() {
-
+  // ************************* Action sheet *************************
+  present() {
+    const actionsheet = this.actionSheetCtrl.create({
+      title: 'Choose image upload',
+      buttons: [
+        {
+          text: 'Choose from gallery',
+          icon: 'image',
+          handler: () => {
+            this.openGallery();
+          },
+        },
+        {
+          text: 'Take picture from camera',
+          icon: 'camera',
+          handler: () => {
+            this.openCamera();
+          },
+        },
+      ],
+    });
+    actionsheet.present();
   }
 
-  findPassengers() {
-    this.findByTag(JSON.stringify(this.ride));
-  }
+  // ************************* Action sheet *************************
 
-  getAllRides(searchParam: SearchParam) {
-    this.ridesArray = this.dataProvider.getRides(searchParam);
-    this.dataProvider.getRides(searchParam).subscribe(
-      (response) => {
+  upload() {
+    const formData = new FormData();
+    formData.append('title', 'getRide');
+    formData.append('description', JSON.stringify(this.rideDetail));
+    formData.append('file', this.rideBlob);
+    this.dataProvider.uploadMedia(formData).subscribe(
+      response => {
         console.log(response);
       },
-      (error) => {
-        console.log(error);
-      });
-  }
-
-  findByTag(tag: string) {
-    this.ridesArray = this.dataProvider.findByTag(tag);
-    this.dataProvider.findByTag(JSON.stringify(this.ride)).subscribe(
-      res => {
-        console.log(res);
-      }
+      err => {
+        console.log(JSON.stringify(err));
+      },
     );
   }
 
+  openCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.filedata = 'data:image/jpeg;base64,' + imageData;
+        this.rideBlob = this.base64ToBlob(this.filedata);
+
+      },
+      err => {
+        console.log(err);
+      },
+    );
+  }
+
+  openGallery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false,
+      targetWidth: 300,
+      targetHeight: 300,
+    };
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.filedata = 'data:image/jpeg;base64,' + imageData;
+        this.rideBlob = this.base64ToBlob(this.filedata);
+      },
+      err => {
+        console.log(err);
+      },
+    );
+  }
+
+  base64ToBlob(dataURL: string) {
+    const byteString = atob(dataURL.split(',')[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ ab ], { type: 'image/jpeg' });
+  }
 }
